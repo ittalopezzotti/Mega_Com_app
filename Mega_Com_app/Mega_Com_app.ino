e6fe27f4b039738c53597453147db3251b60c34b
ITTALO PEZZOTTI ESCOBAR <52321702+ittalopezzotti@users.noreply.github.com>
2023-10-20 15:06:50 +0200

    Version4, 1 HC05 Bluetooth for communications
    The main idea of this version 4 is:
    1) Separate each signal independently, treat it and then send it and
     then separate them and when they work well, put them back together.
    
    2) Take advantage of the serial communication of an electronic card,
     with the objective of proposing necessary communications for the
     management of information; Its configuration is given by 1 HC05 Bluetooth
     for communications via cell phone.

--- Mega_Com_app/Mega_Com_app.ino
+++ Mega_Com_app/Mega_Com_app.ino
@@ -13,7 +13,7 @@
 **Address File   :D:\0_0_2_Repository_GIT_SourceTree\Mega_Com_app******************************************************************
 **File analisis:D:\0_0_2_Repository_GIT_SourceTree\Hasnecood\1_Bluetooth_Mega_Hasnecod_28092023\1_Bluetooth_Mega_Hasnecod_28092023*
 **Name file github:::  E:\1_0_3_Firmware_Publicaciones\Mega_Com_App****************************************************************
-**Version:    Version III *********************************************************************************************************
+**Version:    Version IV *********************************************************************************************************
 *********OBJECTIVE OF THE PROGRAM:::: TRANSMISSION OF SENSOR DATA IN DIFFERENT MODALITIES******************************************
 *********MODALITIES: CELL PHONE, TABLET, SERVER, COMPUTER**************************************************************************
 **********************************************************************************************************************************/
@@ -35,36 +35,43 @@
   ||||||||||||||||||||||||||||||||||||||
   =====================================*/
 void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(9600); //Tx0 and Rx0  //Set Baud Rate to 9600 for Serial Communication Tx0 and Rx0
-  Serial3.begin(9600);  //Tx1 and Rx1  //Connected to Bluetooth Module HC-05 (Bluetooth 2.0)
-  //Serial2;  //Tx2 and Rx2
-  //Serial3;  //Tx3 and Rx3
 
+  Serial1.begin(9600);  //Tx1 and Rx1  //Connected to Bluetooth Module HC-05 (Bluetooth 2.0)
+  Serial.setTimeout(3000); //Set el Timeout en 3 segundos para Serial.find()
+ Serial.println("AT\r"); //PROBAR CON \n\r
+  
+ delay(2000);
+  
+
+  pinMode(13, OUTPUT);  //Set Pin 13 as Output (Connected to LED)
   pinMode(9, OUTPUT);  //Set Pin 13 as Output (Connected to LED)
- 
-//  Serial.println("press \"a\" or \"A\" to switch ON the LED");  //To Desktop
-//  Serial.println("press \"b\" or \"B\" to switch OFF the LED");
-  Serial3.println("press \"a\" or \"A\" to switch ON BLUE  the LED"); //To mobile
-  Serial3.println("press \"b\" or \"B\" to switch OFF BLUE the LED");
+  pinMode(8, OUTPUT);  //Set Pin 13 as Output (Connected to LED)
+
+  Serial1.println("press \"a\" or \"A\" to switch ON the LED"); //To mobile
+  Serial1.println("press \"b\" or \"B\" to switch OFF the LED");
+    delay(500);
+  Serial.flush();       // Clear receive buffer.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  if(Serial3.available()>0)
+  if(Serial1.available()>0)
   {
-    int buffer_value = Serial3.read();
+    int buffer_value = Serial1.read();
     if(buffer_value == 'a' || buffer_value == 'A')
-    {   digitalWrite(9, HIGH);    //Turn ON LED
-//      Serial.println("LED BLU ON");  //Arduino Terminal of Desktop 
-      Serial3.println("LED BLU ON"); //Bluetooth Terminal on Mobile
+    {
+      //digitalWrite(13, HIGH);    //Turn ON LED
+      digitalWrite(9, HIGH);    //Turn ON LED
+//      Serial.println("LED ON");  //Arduino Terminal of Desktop 
+      Serial1.println("LED ON"); //Bluetooth Terminal on Mobile
     }
     else if(buffer_value == 'b' || buffer_value == 'B')
     {
-        digitalWrite(9, LOW);    //Turn ON LED
+     // digitalWrite(13, LOW);      //Turn OFF LED
+      digitalWrite(9, LOW);    //Turn ON LED
 //      Serial.println("LED OFF");  //Arduino Terminal on Desktop
-      Serial3.println("LED BLU OFF"); //Bluetooth Terminal on Mobile 
+      Serial1.println("LED OFF"); //Bluetooth Terminal on Mobile 
     }
   }
 }
