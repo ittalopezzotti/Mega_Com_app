5c6328f7a00c8ea2f147348aa7b526184954866e
ITTALO PEZZOTTI ESCOBAR <52321702+ittalopezzotti@users.noreply.github.com>
2023-10-20 17:27:59 +0200

    SERIAL PORT TEST TABLET/CEL
     /**** START IF FOR SERIAL 3 IN THE COM5 CONECTION TABLET LED BLUE****
    
      /**** START IF FOR SERIAL 2 IN THE COM5 CONECTION TABLET LED YELLOW****/

--- Mega_Com_app/Mega_Com_app.ino
+++ Mega_Com_app/Mega_Com_app.ino
@@ -35,10 +35,18 @@
   ||||||||||||||||||||||||||||||||||||||
   =====================================*/
 void setup() {
+<<<<<<< Updated upstream
 
   Serial1.begin(9600);  //Tx1 and Rx1  //Connected to Bluetooth Module HC-05 (Bluetooth 2.0)
   Serial.setTimeout(3000); //Set el Timeout en 3 segundos para Serial.find()
  Serial.println("AT\r"); //PROBAR CON \n\r
+=======
+  // put your setup code here, to run once:
+  //Serial.begin(9600); //Tx0 and Rx0  //Set Baud Rate to 9600 for Serial Communication Tx0 and Rx0
+   //Serial1.begin(9600);  //Tx1 and Rx1 //Connected to Modem ESP
+   Serial2.begin(9600);  //Tx2 and Rx2 //Connected to Bluetooth Module HC-05 (Bluetooth 2.0)
+   Serial3.begin(9600);  //Tx3 and Rx3 //Connected to Bluetooth Module HC-05 (Bluetooth 2.0)
+>>>>>>> Stashed changes
   
  delay(2000);
   
@@ -47,20 +55,37 @@ void setup() {
   pinMode(9, OUTPUT);  //Set Pin 13 as Output (Connected to LED)
   pinMode(8, OUTPUT);  //Set Pin 13 as Output (Connected to LED)
 
+<<<<<<< Updated upstream
   Serial1.println("press \"a\" or \"A\" to switch ON the LED"); //To mobile
   Serial1.println("press \"b\" or \"B\" to switch OFF the LED");
     delay(500);
   Serial.flush();       // Clear receive buffer.
+=======
+//  Serial.println("press \"a\" or \"A\" to switch ON YELLOW the LED");  //To Desktop
+//  Serial.println("press \"b\" or \"B\" to switch OFF the LED");
+  Serial2.println("press \"a\" or \"A\" to switch ON the YELLOW LED"); //To TABLET
+  Serial2.println("press \"b\" or \"B\" to switch OFF the YELLOW LED");
+  
+  Serial3.println("press \"c\" or \"C\" to switch ON BLUE  the LED"); //To mobile
+  Serial3.println("press \"c\" or \"D\" to switch OFF BLUE the LED");
+
+>>>>>>> Stashed changes
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
+<<<<<<< Updated upstream
   if(Serial1.available()>0)
+=======
+  /**** START IF FOR SERIAL 2 IN THE COM5 CONECTION TABLET LED YELLOW****/
+  if(Serial2.available()>0)
+>>>>>>> Stashed changes
   {
-    int buffer_value = Serial1.read();
+    int buffer_value = Serial2.read();
     if(buffer_value == 'a' || buffer_value == 'A')
     {
+<<<<<<< Updated upstream
       //digitalWrite(13, HIGH);    //Turn ON LED
       digitalWrite(9, HIGH);    //Turn ON LED
 //      Serial.println("LED ON");  //Arduino Terminal of Desktop 
@@ -76,3 +101,34 @@ void loop() {
   }
 }
  
+=======
+      digitalWrite(8, HIGH);    //Turn ON LED
+      Serial2.println("LED YELLOW ON"); //Bluetooth Terminal on TABLET
+    }
+    else if(buffer_value == 'b' || buffer_value == 'B')
+    {
+      digitalWrite(8, LOW);    //Turn ON LED
+      Serial2.println("LED YELLOW OFF"); //Bluetooth Terminal on TABLET 
+    }
+  } //CLOSE SERIAL VARIABLE 2 TABLET
+  
+ /**** START IF FOR SERIAL 3 IN THE COM5 CONECTION TABLET LED BLUE****/
+  if(Serial3.available()>0)
+  {
+    int buffer_value = Serial3.read();
+    if(buffer_value == 'c' || buffer_value == 'C')
+    {
+      digitalWrite(9, HIGH);    //Turn ON LED
+      Serial3.println("LED BLUE ON"); //Bluetooth Terminal on Mobile
+    }
+    else if(buffer_value == 'd' || buffer_value == 'D')
+    {
+      digitalWrite(9, LOW);    //Turn ON LED
+      Serial3.println("LED BLUE OFF"); //Bluetooth Terminal on Mobile 
+    }
+  } //CLOSE SERIAL VARIABLE 3 MOBILE
+
+
+
+}/// CLOSE LOOP 
+>>>>>>> Stashed changes
